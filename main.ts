7d926c2b8c36e216337a6e2417ac209396eb2042
julien launay <julien.launay@laposte.net>
2021-05-07 16:39:42 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
-basic.forever(function () {
-	
-})
+enum list_motor {
+    //% block="M0"
+    M0,
+    //% block="M1"
+    M1,
+    //% block="M2"
+    M2
+}
+
+//% color="#037268" icon="\uf335"
+namespace hoverbit {
+    //% block="Moteur puissance=$power moteur=$name_motor"
+    //% power.min=0 power.max=100
+    //% power.defl=0
+    //% name_motor="M0"
+    export function motor_power(name_motor: list_motor, power: number): void {
+        let rate = power*1023/100;
+        switch (name_motor) {
+            case list_motor.M0:
+                pins.analogWritePin(AnalogPin.P0, rate);
+            case list_motor.M1:
+                pins.analogWritePin(AnalogPin.P1, rate);
+            case list_motor.M2:
+                pins.analogWritePin(AnalogPin.P2, rate);
+        }
+    }
+}
